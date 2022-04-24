c63b39b188b7f2ed18a809fc62aa07d47e880c54
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-04-24 09:46:38 +0300

    ft: add method to save users

--- User.py
+++ User.py
@@ -16,5 +16,16 @@ class User:
 
         self.username = username
         self.password = password
+    
+    def save_User(self):
+
+        """
+        Save_User method save User objects into user_list
+        """
+
+        User.user_list.append(self)
+
+
+        
 
     
